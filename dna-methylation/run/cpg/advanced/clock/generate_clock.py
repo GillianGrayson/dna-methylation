ec26573963ab846653311580fa3ce23a6f4033d2
Unknown <hewitt.archie@yandex.ru>
2019-01-18 01:22:50 +0300

    Introducing strategy and context

---
+++ dna-methylation/run/cpg/advanced/clock/generate_clock.py
@@ -0,0 +1,99 @@
+from lib.config.data.data import *
+from lib.config.setup.setup import *
+from lib.config.annotations.annotations import *
+from lib.config.attributes.attributes import *
+from lib.config.config import *
+from lib.scripts.cpg.advanced.clock.linreg.processing import *
+from lib.setup.advanced.clock.clock import ClockExogType
+
+
+def generate_clock(config_from, config_to):
+    if config_to.setup.method == Method.linreg:
+        generate_clock_linreg(config_from, config_to)
+
+target = 'age'
+
+data = Data(
+    name='cpg_beta',
+    type=DataType.cpg,
+    path='',
+    base='GSE40279'
+)
+
+setup_from = Setup(
+    experiment=Experiment.base,
+    task=Task.table,
+    method=Method.linreg,
+    params={
+        'out_limit': 0.0,
+        'out_sigma': 0.0
+    }
+)
+
+setup_to = Setup(
+    experiment=Experiment.advanced,
+    task=Task.clock,
+    method=Method.linreg,
+    params={
+        'type': ClockExogType.all.value,
+        'exogs': 100,
+        'combs': 100,
+        'runs': 100
+    }
+)
+
+annotations = Annotations(
+    name='annotations',
+    exclude=CommonTypes.none.value,
+    cross_reactive=CrossReactive.exclude.value,
+    snp=SNP.exclude.value,
+    chr=Chromosome.non_gender.value,
+    gene_region=GeneRegion.yes.value,
+    geo=CommonTypes.any.value,
+    probe_class=CommonTypes.any.value
+)
+
+observables = Observables(
+    file_name='observables',
+    types={
+        'gender': CommonTypes.any.value
+    }
+)
+
+cells = Cells(
+    file_name='cells',
+    types=CommonTypes.any.value
+)
+
+attributes = Attributes(
+    observables=observables,
+    cells=cells
+
+)
+
+obs_list = [
+    {'gender': 'F'},
+    {'gender': 'M'},
+    {'gender': CommonTypes.any.value}
+]
+
+for obs in obs_list:
+    attributes.observables.types = obs
+
+    config_from = Config(
+        data=data,
+        setup=setup_from,
+        annotations=annotations,
+        attributes=attributes,
+        target=target
+    )
+
+    config_to = Config(
+        data=data,
+        setup=setup_to,
+        annotations=annotations,
+        attributes=attributes,
+        target=target
+    )
+
+    generate_clock(config_from, config_to)
