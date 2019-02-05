f00c818605790d280f83b7884c936a74458ff92c
Unknown <hewitt.archie@yandex.ru>
2019-02-05 18:42:21 +0300

    Added run script for z_test_linreg

---
+++ dna-methylation/scripts/cpg_proc_table_z_test_linreg.py
@@ -0,0 +1,47 @@
+import pydnameth as pdm
+
+data = pdm.Data(
+    name='cpg_beta',
+    path='',
+    base='GSE87571'
+)
+
+annotations = pdm.Annotations(
+    name='annotations',
+    exclude='none',
+    cross_reactive='ex',
+    snp='ex',
+    chr='NS',
+    gene_region='yes',
+    geo='any',
+    probe_class='any'
+)
+
+observables = pdm.Observables(
+    name='observables',
+    types={'gender': 'vs'}
+)
+
+cells = pdm.Cells(
+    name='cells',
+    types='any'
+)
+
+attributes = pdm.Attributes(
+    target='age',
+    observables=observables,
+    cells=cells
+)
+
+observables_list = [
+    {'gender': 'F'},
+    {'gender': 'M'}
+]
+
+pdm.cpg_proc_table_z_test_linreg(
+    data=data,
+    annotations=annotations,
+    attributes=attributes,
+    observables_list=observables_list,
+    child_method=pdm.Method.linreg
+)
