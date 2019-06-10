717002ebfc19629fdc28b783f50e79fe51bcea9e
Unknown <hewitt.archie@yandex.ru>
2019-06-10 20:40:45 +0300

    Fixed plots

---
+++ dna-methylation/scripts/develop/cells/plot/scatter.py
@@ -0,0 +1,57 @@
+import pydnameth as pdm
+
+data = pdm.Data(
+    path='',
+    base='GSE87571'
+)
+
+annotations = pdm.Annotations(
+    name='annotations',
+    exclude='bad_cpgs',
+    cross_reactive='any',
+    snp='any',
+    chr='NS',
+    gene_region='any',
+    geo='any',
+    probe_class='any'
+)
+
+observables = pdm.Observables(
+    name='observables',
+    types={}
+)
+
+cells = pdm.Cells(
+    name='cells',
+    types='Monocytes'
+)
+
+attributes = pdm.Attributes(
+    target='age',
+    observables=observables,
+    cells=cells
+)
+
+if data.base == 'GSE55763':
+    observables_list = [
+        {'gender': 'F', 'is_duplicate': '0', 'age': (35, 100)},
+        {'gender': 'M', 'is_duplicate': '0', 'age': (35, 100)}
+    ]
+else:
+    observables_list = [
+        {'gender': 'F'},
+        {'gender': 'M'}
+    ]
+
+pdm.cells_plot_scatter(
+    data=data,
+    annotations=annotations,
+    attributes=attributes,
+    observables_list=observables_list,
+    method_params={
+        'x_range': [5, 105],
+        'y_range': 'auto',
+        'legend_size': 1
+    }
+)
+
---
+++ dna-methylation/scripts/develop/entropy/table/z_test_linreg.py
@@ -0,0 +1,69 @@
+import pydnameth as pdm
+
+data = pdm.Data(
+    path='',
+    base='GSE55763'
+)
+
+annotations = pdm.Annotations(
+    name='annotations',
+    exclude='bad_cpgs',
+    cross_reactive='any',
+    snp='any',
+    chr='NS',
+    gene_region='any',
+    geo='any',
+    probe_class='any'
+)
+
+observables = pdm.Observables(
+    name='observables',
+    types={}
+)
+
+if data.base == 'GSE55763':
+    observables_list = [
+        {'gender': 'F', 'is_duplicate': '0', 'age': (35, 100)},
+        {'gender': 'M', 'is_duplicate': '0', 'age': (35, 100)}
+    ]
+
+    data_params = {
+        'data': 'betas_adj',
+        'observables': ['age'],
+        'cells': ['Bcell', 'CD4T', 'NK', 'CD8T', 'Gran']
+    }
+
+    cells = pdm.Cells(
+        name='cells_horvath_calculator',
+        types='any'
+    )
+else:
+    observables_list = [
+        {'gender': 'F'},
+        {'gender': 'M'}
+    ]
+
+    data_params = {
+        'data': 'betas_adj',
+        'observables': ['age'],
+        'cells': ['B', 'CD4T', 'NK', 'CD8T', 'Gran']
+    }
+
+    cells = pdm.Cells(
+        name='cells',
+        types='any'
+    )
+
+attributes = pdm.Attributes(
+    target='age',
+    observables=observables,
+    cells=cells
+)
+
+pdm.entropy_table_z_test_linreg(
+    data=data,
+    annotations=annotations,
+    attributes=attributes,
+    observables_list=observables_list,
+    data_params=data_params,
+)
