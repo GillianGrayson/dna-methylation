9e042562646fa2a9014e64ce136ae4086dcb9834
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2020-03-03 17:34:49 +0300

    added geo table filtering

---
+++ dna-methylation/routines/geo/geo_table_parse.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+geo_file_path = 'E:/YandexDisk/pydnameth/script_datasets/GPL13534/'
+geo_file_name = 'GPL13534_gsm_table.xlsx'
+
+file = geo_file_path + geo_file_name
+table_header = pd.read_excel(file, header=None).iat[0, 0]
+df_geo = pd.read_excel(file, header=0)
+
+source_name = list(df_geo['source_name_ch1'])
+characteristics = list(df_geo['characteristics_ch1'])
+protocol = list(df_geo['extract_protocol_ch1'])
+
+bad_words = ['tumor', 'Tumor', 'fetal', 'Fetal', 'metastasis', 'Metastasis', 'glioma', 'Glioma', 'glioblastoma',
+             'Glioblastoma', 'age (in years): -', 'gestational', 'Gestational', 'injury', 'Injury']
+good_words = ['brain', 'Brain', 'neuron', 'Neuron', 'cortex', 'Cortex', 'cerebellum', 'Cerebellum', 'hippocampus',
+              'Hippocampus', 'lobe', 'Lobe', 'DLPFC', 'glia', 'Glia', 'gyrus', 'Gyrus']
+
+source_name_good_indexes = []
+source_name_bad_indexes = []
+for i in range(0, len(source_name)):
+    item = source_name[i]
+    if isinstance(item, str):
+        for good_word in good_words:
+            if good_word in item:
+                for bad_word in bad_words:
+                    if bad_word in item:
+                        source_name_bad_indexes.append(i)
+                        break
+                if i in source_name_bad_indexes:
+                    break
+                source_name_good_indexes.append(i)
+                break
+
+characteristics_good_indexes = []
+characteristics_bad_indexes = []
+for i in range(0, len(characteristics)):
+    item = characteristics[i]
+    if isinstance(item, str):
+        for good_word in good_words:
+            if good_word in item:
+                for bad_word in bad_words:
+                    if bad_word in item:
+                        characteristics_bad_indexes.append(i)
+                        break
+                if i in source_name_bad_indexes or i in characteristics_bad_indexes:
+                    break
+                characteristics_good_indexes.append(i)
+                break
+
+indexes = list(set(source_name_good_indexes + characteristics_good_indexes))
+indexes.sort()
+df_brain = df_geo.loc[indexes, :]
+
+file_result = geo_file_path + 'brain.xlsx'
+writer = pd.ExcelWriter(file_result, engine='xlsxwriter')
+df_brain.to_excel(writer, index=False, startrow=0)
+worksheet = writer.sheets['Sheet1']
+writer.save()
