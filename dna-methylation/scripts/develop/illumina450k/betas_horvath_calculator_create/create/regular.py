199c3ec0bf12f323754226468d38ce8f929922ef
Archie Hewitt <hewitt.archie@yandex.ru>
2020-12-18 00:59:42 +0300

    proteomic report in progress

--- dna-methylation/scripts/develop/illumina450k/betas_horvath_calculator_create/create/regular.py
+++ dna-methylation/scripts/develop/illumina450k/betas_horvath_calculator_create/create/regular.py
@@ -2,13 +2,21 @@ import pydnameth as pdm
 from scripts.develop.routines import *
 
 data = pdm.Data(
-    path='E:/YandexDisk/Work/pydnameth/tissues/brain(DLPFC)',
+    path='E:/YandexDisk/Work/pydnameth/script_datasets/GPL13534/filtered/brain(DLPFC)',
     base='GSE74193'
 )
 
-data_params = get_data_params(data.base)
+data_params = {
+    'norm': 'fun',
+    'part': 'control',
+}
+
+chip_type = '450k'
+fn = 'observables_part(control)'
 
 pdm.betas_horvath_calculator_create_regular(
+    chip_type=chip_type,
+    observables_fn=fn,
     data=data,
     data_params=data_params
 )
