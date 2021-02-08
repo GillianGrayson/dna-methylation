16115b629359b4c2b81f2318cedd4071cd6e8af2
Archie Hewitt <hewitt.archie@yandex.ru>
2021-02-08 15:53:45 +0300

    again

--- dna-methylation/GEO_parsing/download_data.py
+++ dna-methylation/GEO_parsing/download_data.py
@@ -1,9 +1,9 @@
 import GEOparse
 from GEOparse.utils import download_from_url
-from data.routines.routines import is_float
-from data.infrastructure.path import get_data_path, make_dir
-from data.infrastructure.load.table import load_table_dict_xlsx, load_table_dict_pkl
-from data.infrastructure.save.table import save_table_dict_pkl, save_table_dict_xlsx, save_table_dict_csv
+from functions.routines import is_float
+from GEO_parsing.infrastructure.path import get_data_path, make_dir
+from functions.load.table import load_table_dict_xlsx, load_table_dict_pkl
+from functions.save.table import save_table_dict_pkl, save_table_dict_csv
 import os
 import numpy as np
 from tqdm import tqdm
@@ -13,30 +13,17 @@ import shutil
 import ntpath
 import pickle
 
-na_values = ['', '#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan', '1.#IND', '1.#QNAN', '<NA>',
-             'N/A', 'NA', 'NULL', 'NaN', 'n/a', 'nan', 'null', '-', '--']
 
-def split_words(text):
-    rgx = re.compile(r"((?:(?<!'|\w)(?:\w-?'?)+(?<!-))|(?:(?<='|\w)(?:\w-?'?)+(?=')))")
-    return rgx.findall(text)
-
-def only_words(words):
-    passed_words = []
-    for word in words:
-        if not is_float(word):
-            passed_words.append(word)
-    return passed_words
-
-
-GPL = '21145'
-suffix = '22_09_20'
+GPL = '13534'
+suffix = '03_03_20'
 
 gsm_key = 'gsm'
 gse_key = 'series_id'
 source_key = 'source_name_ch1'
 characteristics_key = 'characteristics_ch1'
 
-target_dir = f'{get_data_path()}/GPL{GPL}/test_single'
+gses = ['GSE87571']
+target_dir = f'{get_data_path()}/GPL{GPL}/filtered/blood(whole)'
 
 fn_xlsx = f'{get_data_path()}/GPL{GPL}/GPL{GPL}_gsm_table_{suffix}.xlsx'
 fn_pkl = f'{get_data_path()}/GPL{GPL}/GPL{GPL}_gsm_table_{suffix}.pkl'
@@ -87,8 +74,6 @@ else:
     save_table_dict_pkl(fn_pkl, gse_gsms_dict)
 
 
-gses = [f.name for f in os.scandir(target_dir) if f.is_dir()]
-
 gse_gsms_passed_dict = {}
 gse_passed_dict = {}
 
@@ -111,7 +96,7 @@ for gse in tqdm(gses):
         try:
             while True:
                 try:
-                    gsm_data = GEOparse.get_GEO(geo=gsm, destdir=f'{path_data}', include_data=True, how="", silent=True)
+                    gsm_data = GEOparse.get_GEO(geo=gsm, destdir=f'{path_data}', include_data=True, how='brief', silent=True)
                     os.remove(f'{path_data}/{gsm}.txt')
 
                     # characteristics processing
--- dna-methylation/functions/routines.py
+++ dna-methylation/functions/routines.py
@@ -1,6 +1,28 @@
+import re
+
+
 def is_float(value):
     try:
         float(value)
         return True
     except ValueError:
         return False
+
+
+def split_words(text):
+    rgx = re.compile(r"((?:(?<!'|\w)(?:\w-?'?)+(?<!-))|(?:(?<='|\w)(?:\w-?'?)+(?=')))")
+    return rgx.findall(text)
+
+
+def only_words(words):
+    passed_words = []
+    for word in words:
+        if not is_float(word):
+            passed_words.append(word)
+    return passed_words
+
+
+def get_na_values():
+    na_values = ['', '#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan', '1.#IND', '1.#QNAN', '<NA>',
+                 'N/A', 'NA', 'NULL', 'NaN', 'n/a', 'nan', 'null', '-', '--']
+    return na_values
