3576f0141e590bc045c815dbb8c7f81090227dcc
Archie Hewitt <hewitt.archie@yandex.ru>
2021-02-24 22:45:44 +0300

    proto

---
+++ dna-methylation/r/pipeline/limma/limma_pipeline.R
@@ -0,0 +1,64 @@
+rm(list=ls())
+
+library(minfi)
+library(limma)
+library(openxlsx)
+
+path = "E:/YandexDisk/Work/pydnameth/script_datasets/GPL13534/filtered/brain(DLPFC)/GSE74193"
+path_wd = paste(path, "/limma", sep='')
+setwd(path_wd)
+
+dataset <- read.table(paste(path, "/betas_part(control)_config(0.01_0.10_0.10)_norm(fun).txt",  sep=''), header = TRUE, sep = "\t")
+
+data <- dataset
+rownames(data) <- data[,1]
+data_b <- data[,-1]
+data_m <- logit2(data_b)
+
+obs<-read.table(paste(path, "/observables_part(control).txt",  sep=''), header=TRUE, sep= "\t")
+
+table(colnames(data_m)==obs$Sample_Name)
+
+Sex <- obs$Sex
+Age <- obs$Age
+
+cells<-read.csv(paste(path, "/cells_counts_part(control).csv",  sep=''), header=TRUE)
+
+CD8T = cells$CD8T
+CD4T = cells$CD4T
+NK = cells$NK
+Bcell = cells$Bcell
+Gran = cells$Gran
+
+propNeuron <- cells$propNeuron
+
+# design <- model.matrix(~Sex+Age, data = data_m)
+# design <- model.matrix(~Sex+Age+propNeuron, data = data_m)
+# design <- model.matrix(~Sex+Age+CD8T+CD4T+NK+Bcell+Gran, data = data_m)
+# design <- model.matrix(~Sex+Age+CD8T+CD4T+NK+Bcell+Gran, data = data_beta)
+# design <- model.matrix(~Sex+Age, data = data_beta)
+design <- model.matrix(~Sex+Age+propNeuron, data = data_m)
+
+
+head(design)
+fit <- lmFit(data_m, design)
+fit2 <- eBayes(fit)
+topl1_Sex <- topTable(fit2, num=Inf,coef="SexM",sort.by="none")
+colnames(topl1_Sex) <- paste0("Sex_",colnames(topl1_Sex))
+topl1_Age <- topTable(fit2, num=Inf,coef="Age",sort.by="none")
+colnames(topl1_Age) <- paste0("Age_",colnames(topl1_Age))
+table(rownames(topl1_Sex)==rownames(topl1_Age))
+results <- data.frame(topl1_Sex,topl1_Age)
+
+write.table(data.frame(results),file=paste(gse, "_beta.txt", sep=''),col.name=TRUE, row.names=TRUE,sep="\t",quote=F)
+
+write.xlsx(results, 
+           paste(path_wd, "/m.xlsx", sep=''), 
+           col.names = TRUE,
+           row.names = TRUE)
+
+# In case we decide to use M values, we have to find good thresholds; just as an example
+dim(results[abs(results$Age_logFC)>0.001 & results$Age_adj.P.Val<0.01,])
+dim(results[abs(results$Sex_logFC)>0.01 & results$Sex_adj.P.Val<0.01,])
+
+
