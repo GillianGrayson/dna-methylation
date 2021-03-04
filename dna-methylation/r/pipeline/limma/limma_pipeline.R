b4e609f7dfe1486adb18ec285e763e720af4627a
Archie Hewitt <hewitt.archie@yandex.ru>
2021-03-04 19:45:46 +0300

    limma liver

--- dna-methylation/r/pipeline/limma/limma_pipeline.R
+++ dna-methylation/r/pipeline/limma/limma_pipeline.R
@@ -6,10 +6,15 @@ library(openxlsx)
 
 path = "E:/YandexDisk/Work/pydnameth/script_datasets/GPL13534/filtered/brain(DLPFC)/GSE74193"
 path = "E:/YandexDisk/Work/pydnameth/script_datasets/GPL13534/filtered/blood(whole)/GSE87571"
+path = "E:/YandexDisk/Work/pydnameth/liver"
+
 path_wd = paste(path, "/limma", sep='')
 setwd(path_wd)
 
 dataset <- read.table(paste(path, "/betas_part(control)_config(0.01_0.10_0.10)_norm(fun).txt",  sep=''), header = TRUE, sep = "\t")
+dataset <- read.table(paste(path, "/betas_part(wo_missedFeatures)_config(0.01_0.10_0.10)_norm(fun).txt",  sep=''), header = TRUE, sep = "\t")
+dataset <- read.csv(paste(path, "/betas_filtered.csv",  sep=''), header = TRUE)
+
 
 data <- dataset
 rownames(data) <- data[,1]
@@ -17,13 +22,26 @@ data_b <- data[,-1]
 data_m <- logit2(data_b)
 
 obs<-read.table(paste(path, "/observables_part(control).txt",  sep=''), header=TRUE, sep= "\t")
+obs<-read.table(paste(path, "/observables_part(wo_missedFeatures).txt",  sep=''), header=TRUE, sep= "\t")
+obs<-read.table(paste(path, "/observables.txt",  sep=''), header=TRUE, sep= "\t")
 
 table(colnames(data_m)==obs$Sample_Name)
+table(colnames(data_m)==obs$geo_accession)
+table(colnames(data_m)==obs$geo_accession)
 
 Sex <- obs$Sex
 Age <- obs$Age
 
+Sex <- obs$gender
+Age <- obs$age
+
+Sex <- obs$gender
+Age <- obs$age
+
 cells<-read.csv(paste(path, "/cells_counts_part(control).csv",  sep=''), header=TRUE)
+cells<-read.csv(paste(path, "/cells_counts_part(wo_missedFeatures).csv",  sep=''), header=TRUE)
+
+table(colnames(data_m)==cells$SampleID)
 
 CD8T = cells$CD8T
 CD4T = cells$CD4T
@@ -33,18 +51,22 @@ Gran = cells$Gran
 
 propNeuron <- cells$propNeuron
 
-# design <- model.matrix(~Sex+Age, data = data_m)
 # design <- model.matrix(~Sex+Age+propNeuron, data = data_m)
 # design <- model.matrix(~Sex+Age+CD8T+CD4T+NK+Bcell+Gran, data = data_m)
 # design <- model.matrix(~Sex+Age+CD8T+CD4T+NK+Bcell+Gran, data = data_beta)
 # design <- model.matrix(~Sex+Age, data = data_beta)
 design <- model.matrix(~Sex+Age+propNeuron, data = data_m)
+design <- model.matrix(~Sex+Age+CD8T+CD4T+NK+Bcell+Gran, data = data_m)
+design <- model.matrix(~Sex+Age, data = data_m)
 
 
 head(design)
 fit <- lmFit(data_m, design)
 fit2 <- eBayes(fit)
 topl1_Sex <- topTable(fit2, num=Inf,coef="SexM",sort.by="none")
+
+topl1_Sex <- topTable(fit2, num=Inf,coef="SexMale",sort.by="none")
+
 colnames(topl1_Sex) <- paste0("Sex_",colnames(topl1_Sex))
 topl1_Age <- topTable(fit2, num=Inf,coef="Age",sort.by="none")
 colnames(topl1_Age) <- paste0("Age_",colnames(topl1_Age))
